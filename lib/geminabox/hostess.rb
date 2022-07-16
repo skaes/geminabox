2fc41add8dac36debbb071652b45eca8f2c79345
Stefan Kaes <stefan.kaes@xing.com>
2022-07-16 10:15:01 +0200

    make code climate happy?

--- lib/geminabox/hostess.rb
+++ lib/geminabox/hostess.rb
@@ -7,40 +7,41 @@ module Geminabox
 
     def self.included(app)
       app.class_eval do
-        %w[/specs.4.8.gz
-           /latest_specs.4.8.gz
-           /prerelease_specs.4.8.gz
-        ].each do |index|
+        %w[/specs.4.8.gz /latest_specs.4.8.gz /prerelease_specs.4.8.gz].each do |index|
           get index do
-            content_type 'application/x-gzip'
-            if Geminabox.rubygems_proxy
-              serve_proxied(Proxy::Splicer.make(index[1..-1]))
-            else
-              serve_local_file
-            end
+            serve_compressed_index(index)
           end
         end
 
         get '/quick/Marshal.4.8/*.gemspec.rz' do
-          content_type('application/x-deflate')
-          if Geminabox.rubygems_proxy
-            serve_proxied(Proxy::Copier.copy(request.path_info[1..-1]))
-          else
-            serve_local_file
-          end
+          serve_gemspec
         end
 
         get "/gems/*.gem" do
-          if Geminabox.rubygems_proxy
-            retrieve_from_rubygems_if_not_local
-          else
-            serve_local_file
-          end
+          serve_gem
         end
       end
     end
 
     private
+
+    def serve_compressed_index(index)
+      content_type 'application/x-gzip'
+      if Geminabox.rubygems_proxy
+        serve_proxied(Proxy::Splicer.make(index[1..-1]))
+      else
+        serve_local_file
+      end
+    end
+
+    def serve_gemspec
+      content_type 'application/x-deflate'
+      if Geminabox.rubygems_proxy
+        serve_proxied(Proxy::Copier.copy(request.path_info[1..-1]))
+      else
+        serve_local_file
+      end
+    end
 
     def serve_local_file
       headers["Cache-Control"] = 'no-transform'
@@ -51,6 +52,14 @@ module Geminabox
     def serve_proxied(file_handler)
       headers["Cache-Control"] = 'no-transform'
       send_file file_handler.proxy_path
+    end
+
+    def serve_gem
+      if Geminabox.rubygems_proxy
+        retrieve_from_rubygems_if_not_local
+      else
+        serve_local_file
+      end
     end
 
     def retrieve_from_rubygems_if_not_local
